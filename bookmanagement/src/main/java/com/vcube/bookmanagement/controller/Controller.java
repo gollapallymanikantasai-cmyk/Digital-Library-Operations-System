86bc547c2aebea6318cbdafc2c61ea589f3fd36f
gollapallymanikantasai-cmyk <gollapallymanikantasai@gmail.com>
2026-04-14 20:01:27 +0530

    Added DockerFile

--- bookmanagement/src/main/java/com/vcube/bookmanagement/controller/Controller.java
+++ bookmanagement/src/main/java/com/vcube/bookmanagement/controller/Controller.java
@@ -3,6 +3,7 @@ package com.vcube.bookmanagement.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vcube.bookmanagement.entity.Modal;
 import com.vcube.bookmanagement.repo.Repository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class Controller {
